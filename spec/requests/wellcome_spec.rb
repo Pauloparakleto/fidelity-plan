647dddf4e0c30ee717932e8e6c41183102322648
paulo-felipe <alparakleto@gmail.com>
2021-10-12 11:47:45 -0300

    refactor(requests/wellcome_spec.rb): check root

--- spec/requests/wellcome_spec.rb
+++ spec/requests/wellcome_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Wellcomes", type: :request do
   describe "GET /welcome" do
     it "returns http success" do
-      get "/wellcome/welcome"
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
