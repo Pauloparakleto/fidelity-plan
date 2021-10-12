4d3dfba224c94508b8188a31d223c4e1efa63b0d
paulo-felipe <alparakleto@gmail.com>
2021-10-12 11:58:11 -0300

    test(models/user.rb): add when create context with password nil and too short checks

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when create" do
+    it "is valid" do
+      user = described_class.create(email: "alparakleto@gmail.com", password: "123456")
+      expect(user).to be_valid
+    end
+
+    it "is invalid password nil" do
+      user = described_class.create(email: "alparakleto@gmail.com", password: nil)
+      expect(user).not_to be_valid
+    end
+
+    it "is invalid password too short" do
+      user = described_class.create(email: "alparakleto@gmail.com", password: "1234")
+      expect(user).not_to be_valid
+    end
+  end
 end
