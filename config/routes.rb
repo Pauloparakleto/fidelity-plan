f19882a6a09afd54d41322132df2efb9f4a2f279
paulo-felipe <alparakleto@gmail.com>
2021-10-15 20:04:22 -0300

    feat(routes.rb): add users index route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     get 'sign_out', to: 'devise/sessions#destroy'
     get 'sign_in', to: 'devise/sessions#new'
   end
+
+  get '/users/index', to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
