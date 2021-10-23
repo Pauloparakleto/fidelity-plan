3a76da5562eaf6c131ec7b0ea6ebf1d84dad6ca4
paulo-felipe <alparakleto@gmail.com>
2021-10-23 12:13:00 -0300

    fix(requests/user_spec.rb): add found home to non logged admin

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    it "gets index" do
+    it "does not get index" do
       get "/users/index"
       expect(response).to have_http_status(:ok)
     end
