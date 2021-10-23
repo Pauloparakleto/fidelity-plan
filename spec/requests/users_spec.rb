1c1cd237622963cab8446719ab384e5572ecdb49
paulo-felipe <alparakleto@gmail.com>
2021-10-23 12:13:45 -0300

    test(requests/user_spec.rb): add get index to admin signed in

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "Users", type: :request do
+  let!(:admin) { create(:admin) }
+
   describe "GET /index" do
     it "does not get index" do
+      get "/users/index"
+      expect(response).to have_http_status(:found)
+    end
+
+    it "gets index" do
+      sign_in admin
       get "/users/index"
       expect(response).to have_http_status(:ok)
     end
