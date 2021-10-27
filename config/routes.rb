7959193351c8d0a9b9559793851a24d8b8667ce5
paulo-felipe <alparakleto@gmail.com>
2021-10-27 08:49:48 -0300

    feat(routes.rb): remove sign up path from admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, :skip => [:registration]
+  as :admin do
+    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+    patch 'admins' => 'devise/registrations#update', :as => 'admin_registration'
+  end
   devise_for :users
   root to: 'wellcome#welcome'
 
