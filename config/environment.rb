de4b7fcd36765867d8bb662183088035c3a7d3ad
paulo-felipe <alparakleto@gmail.com>
2021-11-12 19:52:13 -0300

    chore(config/environment.rb): add point action mailer to sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => Rails.application.credentials.sendgrid[:password], # This is the secret sendgrid API key which was issued during API key creation
+  :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
   :domain => 'fidelity-plan.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
