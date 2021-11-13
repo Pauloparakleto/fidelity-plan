586c0bbbd669c068271fb5ef032c2e574a13becb
paulo-felipe <alparakleto@gmail.com>
2021-11-13 20:26:12 -0300

    chore(production.rb): user name apikey

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     # TODO, Check the user name
-    :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+    :user_name => 'fidelity-plan-sendgrid', # This is the string literal 'apikey', NOT the ID of your API key
     :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
     :domain => 'fidelity-plan.com',
     :address => 'smtp.sendgrid.net',
