8ceeee0151f0010031d9b0c05a32a2a3fadf0d2e
paulo-felipe <alparakleto@gmail.com>
2021-11-27 18:51:50 -0300

    refactor(lib/whatsapp/messages.rb): use self also in private methods

--- app/lib/whatsapp/messages.rb
+++ app/lib/whatsapp/messages.rb
@@ -1,14 +1,18 @@
 module Whatsapp
   class Messages
     def self.one_way_message
-      account_sid = ENV['TWILIO_ACCOUNT_ID']
-      auth_token = ENV['TWILIO_AUTH_TOKEN']
+      account_sid = ENV["TWILIO_ACCOUNT_ID"]
+      auth_token = ENV["TWILIO_AUTH_TOKEN"]
       @client = Twilio::REST::Client.new(account_sid, auth_token)
 
+      create_message
+    end
+
+    def self.create_message
       @client.messages.create(
-        body: 'Aqui é do Godzilla lanches!',
-        from: 'whatsapp:+14155238886',
-        to: 'whatsapp:+559192736624'
+        body: "Aqui é do Godzilla lanches!",
+        from: "whatsapp:+14155238886",
+        to: "whatsapp:+559192736624",
       )
     end
   end
