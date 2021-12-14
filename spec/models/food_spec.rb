cd0af23d37e7e0fb5ec8dd2bf12fdf358d7e9e64
paulo-felipe <alparakleto@gmail.com>
2021-12-13 22:55:15 -0300

    test(spec/models/food.rb): add validations and refactor

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,49 +1,50 @@
 require "rails_helper"
 
 RSpec.describe Food, type: :model do
+  let!(:food) { create(:food) }
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
+
   context "when create" do
     it "is valid" do
-      food = create(:food)
       expect(food).to be_valid
     end
 
     it "has name" do
-      food = create(:food)
       expect(food.name).to eq("x-bacon")
     end
 
     it "has description" do
-      food = create(:food)
       expect(food.description).to eq("salad, tomato")
     end
 
     it "has price" do
-      food = create(:food)
       expect(food.price).to eq(12.00)
     end
   end
 
   context "when update" do
     it "is valid" do
-      food = create(:food)
       food.update(name: "x-salad")
       expect(food).to be_valid
     end
 
     it "has name" do
-      food = create(:food)
       food.update(name: "x-salad")
       expect(food.name).to eq("x-salad")
     end
 
     it "has description" do
-      food = create(:food)
       food.update(description: "salad")
       expect(food.description).to eq("salad")
     end
 
     it "has price" do
-      food = create(:food)
       food.update(price: 13.00)
       expect(food.price).to eq(13.00)
     end
