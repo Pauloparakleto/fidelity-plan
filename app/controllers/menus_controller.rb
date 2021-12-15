f560f692d097c379cdf90fea0fa5892da1cbfe55
paulo-felipe <alparakleto@gmail.com>
2021-12-15 17:13:44 -0300

    feat(menus_controller.rb): add update image action

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,7 +3,19 @@ class MenusController < ApplicationController
 
   def show; end
 
+  def update_image
+    menu_param = menu_params.to_h
+    food_image = menu_param[:food_image]
+    @menu.food_image.attach(food_image)
+
+    redirect_to menu_path, notice: "Food Image Uploaded!"
+  end
+
   private
+
+  def menu_params
+    params.require(:menu).permit(:food_image)
+  end
 
   def set_menu
     @menu = Menu.first
