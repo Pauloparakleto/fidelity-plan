ef4f88f5b00fa12a34f47a9423ca2e07ac7e5b76
paulo-felipe <alparakleto@gmail.com>
2021-12-15 17:07:05 -0300

    test(spec/requests/menus_spec.rb): add patch image upload menu

--- spec/requests/menus_spec.rb
+++ spec/requests/menus_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe "menu", type: :request do
     get menu_path
     expect(response).to be_successful
   end
+
+  it "PATCH image" do
+    food_image = fixture_file_upload("menu_food.jpeg")
+    menu = create(:menu)
+    patch update_food_image_path(menu.id), params: { menu: { food_image: food_image } }
+    expect(menu.reload.food_image).to be_attached
+  end
 end
-
-
