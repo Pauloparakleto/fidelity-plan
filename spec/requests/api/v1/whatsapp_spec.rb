2c83eed340d8c86d39f3e94f6f037454ef62ec0e
paulo-felipe <alparakleto@gmail.com>
2021-12-17 13:11:28 -0300

    refactor(messages_spec.rb): remove unecessary params

--- spec/requests/api/v1/whatsapp_spec.rb
+++ spec/requests/api/v1/whatsapp_spec.rb
@@ -2,20 +2,13 @@ require "rails_helper"
 
 RSpec.describe "whatsapp", type: :request do
   let!(:params) {
-    { "SmsMessageSid" => "SM0424861ae52314da930b962ac859d87d",
-      "NumMedia" => "0",
-      "ProfileName" => "Paulo Felipe",
-      "SmsSid" => "SM0424861ae52314da930b962ac859d87d",
+    { "ProfileName" => "Paulo Felipe",
       "WaId" => "559192736624",
-      "SmsStatus" => "received",
-      "Body" => "F",
       "To" => "whatsapp:+14155238886",
-      "NumSegments" => "1",
-      "MessageSid" => "SM0424861ae52314da930b962ac859d87d",
-      "AccountSid" => "ACf9adc904b3231e0d15d52b52a7862648",
       "From" => "whatsapp:+559192736624",
       "ApiVersion" => "2010-04-01" }
   }
+
   it "gets one way message" do
     get api_v1_send_one_way_message_path
     expect(response).to have_http_status(:ok)
