3fa3e33070c01e1f23241649ee8d3a93ba97d4ab
paulo-felipe <alparakleto@gmail.com>
2021-12-17 13:01:46 -0300

    feat(whatsapp/messages.rb): create and save user phone

--- app/lib/whatsapp/messages.rb
+++ app/lib/whatsapp/messages.rb
@@ -1,10 +1,28 @@
 module Whatsapp
   class Messages
-    def initialize
+    def initialize(params)
+      @params = params
       @response = Twilio::TwiML::MessagingResponse.new
     end
 
+    def create_user
+      User.create(email: set_email, password: set_password, phone: @params['WaId'])
+    end
+
+    def set_email
+      name = @params['ProfileName'].downcase.gsub(" ", "_")
+      number = @params['WaId'].delete("55")
+      name.concat("_", number, "@email.com")
+    end
+
+    def set_password
+      name = @params['ProfileName'].downcase.gsub(" ", "_")
+      number = @params['WaId'].delete("55")
+      name.concat("_", number)
+    end
+
     def bot
+      create_user
       @response.message do |message|
         message.body("Esse é o nosso cardápio tradicional...")
         message.media path_to_food_menu
