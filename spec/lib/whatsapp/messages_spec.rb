8f87cfbc0eabcd74e15b1edcc0c6751e4732b1a3
paulo-felipe <alparakleto@gmail.com>
2021-12-17 13:11:18 -0300

    refactor(messages_spec.rb): remove unnecessary params

--- spec/lib/whatsapp/messages_spec.rb
+++ spec/lib/whatsapp/messages_spec.rb
@@ -2,17 +2,9 @@ require "rails_helper"
 
 RSpec.describe Whatsapp::Messages do
   let!(:params) {
-    { "SmsMessageSid" => "SM0424861ae52314da930b962ac859d87d",
-      "NumMedia" => "0",
-      "ProfileName" => "Paulo Felipe",
-      "SmsSid" => "SM0424861ae52314da930b962ac859d87d",
+    { "ProfileName" => "Paulo Felipe",
       "WaId" => "559192736624",
-      "SmsStatus" => "received",
-      "Body" => "F",
       "To" => "whatsapp:+14155238886",
-      "NumSegments" => "1",
-      "MessageSid" => "SM0424861ae52314da930b962ac859d87d",
-      "AccountSid" => "ACf9adc904b3231e0d15d52b52a7862648",
       "From" => "whatsapp:+559192736624",
       "ApiVersion" => "2010-04-01" }
   }
@@ -24,7 +16,7 @@ RSpec.describe Whatsapp::Messages do
     described_class.new(params).bot
 
     expect(User.last.phone).to eq("559192736624")
-    end
+  end
 
   it "set email" do
     email = described_class.new(params).set_email
