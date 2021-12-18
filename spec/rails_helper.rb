2ac603d7016e2c21e9e9e4aae15097741578bf50
paulo-felipe <alparakleto@gmail.com>
2021-12-18 13:45:59 -0300

    chore(fixture/files/menu_food.rb): add image to correct rails new version fixture

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,4 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+# This files is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'devise'
 ENV['RAILS_ENV'] ||= 'test'
@@ -45,7 +45,7 @@ RSpec.configure do |config|
   # config.use_active_record = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
+  # based on their files location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
   #
   # You can disable this behaviour by removing the line below, and instead
