869a00d43e6c11ea02bbe8935bb1f389f283da42
paulo-felipe <alparakleto@gmail.com>
2021-12-20 12:07:03 -0300

    feat(models/order_spec.rb): has two items, food and drink initial one to many nested

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe Order, type: :model do
       order = described_class.create(item_attributes: { food_id: food_menu.id })
       expect(order.item).to eq(Item.last)
     end
+
+    it "has two items" do
+      food = create(:food)
+      drink = create(:drink)
+      order = described_class.create(items_attributes: [{ food_id: food.id },
+                                                        { drink_id: drink.id }])
+      expect(order.items.count).to eq(2)
+    end
   end
 end
