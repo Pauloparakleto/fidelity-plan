217469e3d01aa5c69908638dfa13d672a8504445
paulo-felipe <alparakleto@gmail.com>
2021-12-20 13:19:41 -0300

    refactor(spec/models/order_spec.rb): add name

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,14 +5,22 @@ RSpec.describe Order, type: :model do
   let!(:drink) { create(:drink) }
   let!(:order) {
     described_class.create(items_attributes: [
-                             { food_id: food.id, price: 13.23, quantity: 2 },
-                             { drink_id: drink.id, price: 4.50, quantity: 3 },
+                             { food_id: food.id, name: food.name, price: 13.23, quantity: 2 },
+                             { drink_id: drink.id, name: drink.name, price: 4.50, quantity: 3 },
                            ])
   }
 
   context "when create" do
+    it "counts order" do
+      expect(described_class.count).to eq(1)
+    end
+
     it "has two items" do
       expect(order.items.count).to eq(2)
+    end
+
+    it "has first item with name" do
+      expect(order.items.first.name).to eq(food.name)
     end
 
     it "has first item with price" do
@@ -21,6 +29,10 @@ RSpec.describe Order, type: :model do
 
     it "has first item with quantity" do
       expect(order.items.first.quantity).to eq(2)
+    end
+
+    it "has second item with name" do
+      expect(order.items.second.name).to eq(drink.name)
     end
 
     it "has second item with price" do
