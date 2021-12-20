3a6040e0bbb27282c60859fcd8206eb4e5479e63
paulo-felipe <alparakleto@gmail.com>
2021-12-20 14:39:57 -0300

    feat(models/order.rb): add set total order before sabe

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,18 @@
 class Order < ApplicationRecord
+  attr_accessor :total
+
   has_many :items, dependent: :destroy
   accepts_nested_attributes_for :items
+
+  before_save :set_total
+
+  private
+
+  def set_total
+    @total = 0
+    items.each do |item|
+      @total += item.quantity*item.price
+    end
+    @total
+  end
 end
