efe1f2acae495a7dbb29a61deb548026301b5f9b
paulo-felipe <alparakleto@gmail.com>
2021-12-20 12:20:09 -0300

    refactor(spec/models/order_spec.rb): add let version

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
+  let!(:food) { create(:food) }
+  let!(:drink) { create(:drink) }
+  let!(:order) {
+    described_class.create(items_attributes: [
+                             { food_id: food.id, price: 13.23, quantity: 2 },
+                             { drink_id: drink.id, price: 4.50, quantity: 3 },
+                           ])
+  }
+
   context "when create" do
-    it "is valid" do
-      food_menu = create(:food)
-      order = described_class.create(item_attributes: { food_id: food_menu.id })
-      expect(order.item).to eq(Item.last)
+    it "has two items" do
+      expect(order.items.count).to eq(2)
     end
 
-    it "has two items" do
-      food = create(:food)
-      drink = create(:drink)
-      order = described_class.create(items_attributes: [{ food_id: food.id },
-                                                        { drink_id: drink.id }])
-      expect(order.items.count).to eq(2)
+    it "has first item with price" do
+      expect(order.items.first.price).to eq(13.23)
+    end
+
+    it "has first item with quantity" do
+      expect(order.items.first.quantity).to eq(2)
+    end
+
+    it "has second item with price" do
+      expect(order.items.second.price).to eq(4.50)
+    end
+
+    it "has second item with quantity" do
+      expect(order.items.second.quantity).to eq(3)
     end
   end
 end
