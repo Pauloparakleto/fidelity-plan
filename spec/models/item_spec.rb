ea4acf96a9f83036a7185b152c9fe8afee00856f
paulo-felipe <alparakleto@gmail.com>
2021-12-22 00:35:50 -0300

    refactor(spec/models/item_spec.rb): remove test out of business rule

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,15 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Item, type: :model do
-  context "when create valid" do
-    it "has valid order item" do
-      food = create(:food)
-      order = Order.items.build(food_id: food.id, quantity: 2)
-      order.save
-      expect(order.items.first).to be_truthy
-    end
-  end
-
   context "when create invalid" do
     it "has invalid quantity" do
       food = create(:food)
